7aa4caeecfcf53fec77d72cbb69b9690af9d65fb
Kailin Li <35666977+kailinandcode@users.noreply.github.com>
2018-03-06 11:17:03 -0800

    Create overdue.ino

---
+++ overdue.ino
@@ -0,0 +1,8 @@
+/* You are an AI agent on a near-future iPhone tasked with communicating the content
+of each of the following notifications messages using a single LED.
+An engineer might immediately think "easy-peasy, I'll just use Morse code!"
+-- but this response doesn't consider that your user is unlikely to visually parse
+(or even know of) Morse code. Think instead of what piece of the message you could
+reasonably get across with a single pixel; how might you communicate urgency? Or compassion? */
+
+//"Your homework assignment is overdue." 
