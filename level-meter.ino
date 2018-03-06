5d7a0eaec2b3426371cf4ff2f7ed597e97cbaba1
Kailin Li <35666977+kailinandcode@users.noreply.github.com>
2018-03-05 16:45:57 -0800

    Create level-meter.ino

---
+++ level-meter.ino
@@ -0,0 +1,7 @@
+/*implements a "level meter" using these three LEDs. 
+When the potentionmeter is turned all the way counter-clockwise, all LEDs should be off. 
+As you turn it clockwise, each subsequent LED should turn on as the rotation passes 1/4, 2/4, and 3/4 of the way around.
+You can base your code on the Analog Input sketch we used in class
+(found in the Arduino IDE under File > Examples > Analog > AnalogInput). 
+You won't need blinking, but you will need an if statement
+that checks the sensorValue to see how rotated your potentiometer is.*/
