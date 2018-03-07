298edbfa5669ea049180d37043dd64e0ef8064b5
Kailin Li <35666977+kailinandcode@users.noreply.github.com>
2018-03-06 20:55:01 -0800

    Update level-meter.ino

--- level-meter.ino
+++ level-meter.ino
@@ -5,3 +5,36 @@ You can base your code on the Analog Input sketch we used in class
 (found in the Arduino IDE under File > Examples > Analog > AnalogInput). 
 You won't need blinking, but you will need an if statement
 that checks the sensorValue to see how rotated your potentiometer is.*/
+
+int sensorPin = A0;    // select the input pin for the potentiometer
+int ledPin1 = 13;   
+int ledPin2 = 12;
+int ledPin3 = 11;
+// select the pin for the LED
+int sensorValue = 0;  // variable to store the value coming from the sensor
+
+void setup() {
+  // declare the ledPin as an OUTPUT:
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  // read the value from the sensor:
+  sensorValue = analogRead(sensorPin);
+  // stop the program for <sensorValue> milliseconds:
+  delay(sensorValue);
+  // write an if statement to check the sensorValue
+  if (sensorValue == 1230/4 ) {
+    digitalWrite(ledPin1, HIGH);
+  }
+  if (sensorValue == 3*1230/4) {
+     digitalWrite(ledPin1, LOW);
+     digitalWrite(ledPin2, HIGH);
+  }
+  if (sensorValue == 2*1230/4) {
+     digitalWrite(ledPin2, LOW);
+     digitalWrite(ledPin3, HIGH);
+  }
+  // stop the program for for <sensorValue> milliseconds:
+  delay(sensorValue);
+}
