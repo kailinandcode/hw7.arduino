6190bc580d772d382cc4f635eb3a01662cef325b
Kailin Li <35666977+kailinandcode@users.noreply.github.com>
2018-03-06 22:47:22 -0800

    Update meeting.ino

--- meeting.ino
+++ meeting.ino
@@ -5,3 +5,18 @@ int sensorPin = A0;    // select the input pin for the potentiometer
 int ledPin = 13;      // select the pin for the LED
 int sensorValue = 0;  // variable to store the value coming from the sensor
 
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  sensorValue = analogRead(sensorPin);
+  // flash for every minute with particular delay, for five times
+  for (i = 0; i < 5; i++) {
+    digitalWrite(ledPin, HIGH);
+    delay(100);
+    digitalWrite(ledPin, LOW);
+  }
+  delay(sensorValue);
+}
