684caf56d553b6aaa80888b7ec4971dea6ea3b30
Kailin Li <35666977+kailinandcode@users.noreply.github.com>
2018-03-06 22:00:20 -0800

    Update mothers-love.ino

--- mothers-love.ino
+++ mothers-love.ino
@@ -1,1 +1,29 @@
 //"Mom says: love you sweetie, have a great day!" 
+
+//use a sensor value larger to represent the mom's age (to the child's.)
+
+
+int sensorPin = A0;    // select the input pin for the potentiometer
+int ledPin = 13;      // select the pin for the LED
+int sensorValue = 0;  // variable to store the value coming from the sensor
+int mom = 500;
+int child = 50;
+int sentenceEnd = 0;
+
+void setup() {
+  // declare the ledPin as an OUTPUT:
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  // read the value from the sensor:
+  sensorValue = analogRead(sensorPin);
+  // turn the ledPin on
+  digitalWrite(ledPin, HIGH);
+  // stop the program for <sensorValue> milliseconds:
+  delay(mom);
+  // turn the ledPin off:
+  digitalWrite(ledPin, LOW);
+  // stop the program for for <sensorValue> milliseconds:
+  delay(child);
+}
