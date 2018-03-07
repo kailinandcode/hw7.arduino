f69acc14ccf9b5b67282fa840e22b4164d56ba1c
Kailin Li <35666977+kailinandcode@users.noreply.github.com>
2018-03-06 23:16:47 -0800

    Update wrong-turn.ino

--- wrong-turn.ino
+++ wrong-turn.ino
@@ -1,1 +1,22 @@
 //"You are following directions, but you took a wrong turn."
+
+int sensorPin = A0;
+int ledPin = 13;
+int sensorValue = 0;
+
+//warns continuously
+
+int warningTime = 0;
+
+void setup() {
+  pinMOde(ledPin, OUTPUT);
+}
+
+void loop() {
+  sensorValue - analogRead(sensorPin);
+  while (warningTime < 10) {
+    digitalWrite(ledPin, HIGH)
+  }
+  delay(10);
+  warningTime += 1;
+}
